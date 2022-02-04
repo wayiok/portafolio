de75f0e0c39972a4e04081cd19f0a6e6ed73ce16
wayiok <cesar.anasco@hotmail.com>
2022-02-03 20:25:46 -0500

    New pages added
    Images changed and TECH. Button added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,4 +120,16 @@
 					visibleClass: 'header-visible'
 				});
 
+
+				$('.button').click(function(){
+  var buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
+
 })(jQuery);
